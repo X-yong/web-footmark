236fb203f0e988f926fcdeff5e453e65812c9d07
xiongyong <yong.xiong@inzymeits.com>
2018-10-09 20:25:50 +0800

    fix #001  初始化提交

---
+++ src/main/java/com/web/footmark/FootmarkApplication.java
@@ -0,0 +1,12 @@
+package com.web.footmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootmarkApplication.class, args);
+    }
+}
---
+++ src/test/java/com/web/footmark/FootmarkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.web.footmark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootmarkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
