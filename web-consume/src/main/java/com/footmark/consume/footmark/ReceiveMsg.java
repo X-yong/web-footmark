09b339059664acf99c3466259ec4a43c94329566
xiongyong <yong.xiong@inzymeits.com>
2019-01-25 17:03:02 +0800

    feat 使用阿里nacos替换eureke

--- web-consume/src/main/java/com/footmark/consume/footmark/ReceiveMsg.java
+++ web-consume/src/main/java/com/footmark/consume/footmark/ReceiveMsg.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Component;
  * @Date : 2018/10/12 11:08
  */
 @Component
-@RabbitListener(queues = "test")
+//@RabbitListener(queues = "test")
 public class ReceiveMsg {
-    @RabbitHandler
+    //@RabbitHandler
     public void receive(User content) {
 
         System.out.println("接收到消息: " + content.getName());
